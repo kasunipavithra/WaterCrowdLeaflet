ab2f5c55c20e3eb2f75f396e47f29ce22d92e25d
kasunipavithra <wskasunipavithra@gmail.com>
2018-10-14 15:05:24 +0530

    some refactoring

--- src/app/map/map-panel/map-panel.component.ts
+++ src/app/map/map-panel/map-panel.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { latLng, tileLayer,Map, Layer, marker,icon } from 'leaflet';
-//import { MapPointService } from '../../services/map-point.service';
-
 
 @Component({ 
   selector: 'app-map-panel',
@@ -11,33 +9,15 @@ import { latLng, tileLayer,Map, Layer, marker,icon } from 'leaflet';
 
 export class MapPanelComponent implements OnInit {
 
-
-  constructor(private zone: NgZone) { }
+constructor(private zone: NgZone) { }
 
 fitBounds: any = null;
-/*circle = circle([ 46.95, -122 ], { radius: 5000 });
-*/
-  ngOnInit() {
-  
-    /*
-	// The 'add' event callback handler happens outside of the Angular zone
-	this.circle.on('add', () => {
-		
-		// But, we can run stuff inside of Angular's zone by calling NgZone.run()
-		// everything inside the arrow function body happens inside of Angular's zone, where changes will be detected
-		this.zone.run(() => {
-			this.fitBounds = this.circle.getBounds();
-		});
-		
-  });
-  
-  */
-  }
 
-  onMapReady(map: Map) {
+ngOnInit() {}
+
+onMapReady(map: Map) {
     map.on('click', <LeafletMouseEvent>(e) => { console.log(e.latlng.lat,e.latlng.lng);
-    
-    
+      //run click event on zone 
     	this.zone.run(() => {
         this.fitBounds = this.addMarker(e.latlng.lat,e.latlng.lng);
       });
@@ -45,30 +25,15 @@ fitBounds: any = null;
     });
   }
 
-
-  // Marker for the top of Mt. Ranier
-  summit = marker([ 46.8523, -121.7603 ], {
-    icon: icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
-      iconUrl: 'leaflet/marker-icon.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
-  });
-
-
-
 // Open Street Map definitions
 LAYER_OSM = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' });
 
 // Values to bind to Leaflet Directive
 options = {
-  layers: [ this.LAYER_OSM , this.summit],
+  layers: [ this.LAYER_OSM],
   zoom: 10,
   center: latLng(46.879966, -121.726909)
 };
-
-
 
 
 markers: Layer[] = [];
@@ -92,6 +57,5 @@ addMarker(latitude:number,longitude:number) {
 removeMarker() {
   this.markers.pop();
 }
-
 
 }
