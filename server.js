a12d7027269b07a3538c185a18c11a75a51235bf
kasunipavithra <wskasunipavithra@gmail.com>
2018-10-15 23:21:40 +0530

    imports are chnged to es6

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/project4'));
+app.use(static(__dirname + '/dist/project4'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/project4/index.html'));
+res.sendFile(join(__dirname+'/dist/project4/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
